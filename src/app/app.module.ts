aee5d8f18d1546b26b43c3631aa45d8f6441ffdf
Francisco Briceño <francisco.briceno.a@mail.pucv.cl>
2021-04-27 12:58:51 -0400

    add creator, header and list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CreatorComponent } from './creator/creator.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CreatorComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
